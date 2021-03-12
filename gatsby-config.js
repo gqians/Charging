beddadd7e2c83a0350c7f8cd4068e513962e8772
Qian4321 <1973078177@qq.com>
2021-03-13 00:00:52 +0800

    更新

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,9 @@ module.exports = {
       twitter: `kylemathews`,
     },
   },
+	siteMetadata: {
+		siteUrl: `http://www.qds.ink`,
+	},
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -111,7 +114,43 @@ module.exports = {
       options: {
         templatePath: `${__dirname}/src/templates/tags/index.jsx`,
       },
-},
+		},
+		{
+			resolve: `gatsby-plugin-sitemap`,
+			options: {
+				output: `/sitemap.xml`,
+				// Exclude specific pages or groups of pages using glob parameters
+				// See: https://github.com/isaacs/minimatch
+				// The example below will exclude the single `path/to/page` and all routes beginning with `category`
+				exclude: [`/category/*`, `/path/to/page`],
+				query: `
+					{
+						wp {
+							generalSettings {
+								siteUrl
+							}
+						}
+	
+						allSitePage {
+							nodes {
+								path
+							}
+						}
+				}`,
+				resolveSiteUrl: ({site, allSitePage}) => {
+					//Alternatively, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+					return site.wp.generalSettings.siteUrl
+				},
+				serialize: ({ site, allSitePage }) =>
+					allSitePage.nodes.map(node => {
+						return {
+							url: `${site.wp.generalSettings.siteUrl}${node.path}`,
+							changefreq: `daily`,
+							priority: 0.7,
+						}
+					})
+			}
+		}
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
