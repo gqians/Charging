8960ee72b5386c51f763907932fa0d5b7f91f2ef
gqians <qhong1250@gmail.com>
2022-05-16 09:40:46 +0800

    更新

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,34 +7,42 @@ import "./src/styles/global.css"
 // import Blobity from "blobity"
 import  config from './src/utils/config'
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
-Object.entries(config.theme).forEach(([key, value]) => {
+export const onClientEntry = () => {
+	Object.entries(config.theme).forEach(([key, value]) => {
     document.documentElement.style.setProperty(key, value);
-});
-if(window.localStorage){
-	const qdsThemeColor = window.localStorage;
-	const content = qdsThemeColor.getItem('qdsThemeColor')
-	if(content){
-		const qdsThemeColor = JSON.parse(content)
-		const htmlElement = document.getElementsByTagName('html')[0]
-		htmlElement.style.setProperty("--color-h",qdsThemeColor['--color-h']);
-		htmlElement.style.setProperty("--color-s",qdsThemeColor['--color-s']);
-		htmlElement.style.setProperty("--color-l",qdsThemeColor['--color-l']);
+	});
+	if(window.localStorage){
+		const qdsThemeColor = window.localStorage;
+		const content = qdsThemeColor.getItem('qdsThemeColor')
+		if(content){
+			const qdsThemeColor = JSON.parse(content)
+			const htmlElement = document.getElementsByTagName('html')[0]
+			htmlElement.style.setProperty("--color-h",qdsThemeColor['--color-h']);
+			htmlElement.style.setProperty("--color-s",qdsThemeColor['--color-s']);
+			htmlElement.style.setProperty("--color-l",qdsThemeColor['--color-l']);
+		}
 	}
 }
-// const threeScript = document.createElement('script');
-// threeScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/three.js/96/three.min.js';
-// document.body.appendChild(threeScript);
-// const script = document.createElement('script');
-// script.src = 'https://cdnjs.cloudflare.com/ajax/libs/three.js/96/three.min.js';
-// document.body.appendChild(script);
-{/* <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/three.js/96/three.min.js"></script>
-			<script type="text/javascript" src="https://www.vantajs.com/dist/vanta.birds.min.js"></script> */}
+export const onRouteUpdate = (args, pluginOptions) => {
+  const token = "1402e55e74034ea7bf9c551253147779";
 
-// const options = { 
-// 			color: 'rgb(180, 180, 180)',
-// 			invert: true,
-// 			opacity: 0.1
-// 		};
-// new Blobity(options);
+  if(!token || typeof token != 'string') {
+    throw Error('gatsby-plugin-cloudflare-web-analytics: No token provided in gatsby-config.js');
+  }
+  
+  const head = document.head;
+  const script = document.createElement('script');
+  script.id = 'cloudflare-web-analytics'
+  script.defer = true;
+  script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+  script.setAttribute('data-cf-beacon', `{"token": "${token}"}`);
+
+  const existingScript = head.querySelector(`#${script.id}`);
+  if(existingScript) {
+    head.removeChild(existingScript)
+  }
+
+  head.appendChild(script);
+}
 
 deckDeckGoHighlightElement();
